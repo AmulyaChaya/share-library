9a941ad1a80364904cce21c1ea5961d89b5a10a9
AmulyaChaya <amulyahn2012@gmail.com>
2021-07-21 17:16:16 +0530

    Update sendoffUser.groovy

--- vars/sendoffUser.groovy
+++ vars/sendoffUser.groovy
@@ -2,14 +2,14 @@ def call(String username = 'null', String age = 'null') {
 	echo "This is shared library sendoffUser: ${username}"
 	pipeline {
 		environment {
-			USER_NAME = "$username"
-			USER_AGE = "$age"
+			USER_NAME = "${username}"
+			USER_AGE = "${age}"
 		}
 		agent any 
 		stages {
 			stage('SENDOF_USER') {
 				steps {
-					echo "Bye USER_NAME, Nice meeting you !"
+					echo "Bye $USER_NAME, Nice meeting you !"
 				}	
 			}
 			stage('satge2') {
