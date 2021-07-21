086fc17a9798861168e231dde9789c7718cb7b29
AmulyaChaya <amulyahn2012@gmail.com>
2021-07-21 17:10:13 +0530

    Update sendoffUser.groovy

--- vars/sendoffUser.groovy
+++ vars/sendoffUser.groovy
@@ -9,12 +9,12 @@ def call(String username = 'null', String age = 'null') {
 		stages {
 			stage('SENDOF_USER') {
 				steps {
-					echo "Bye $USER_NAME, Nice meeting you !"
+					echo "Bye ${username}, Nice meeting you !"
 				}	
 			}
 			stage('satge2') {
 				steps {
-					echo "User Age: $USER_AGE"	
+					echo "User Age: ${age}"	
 				}		
 			}	
 		}
