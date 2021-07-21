4dc3db614831cbf3226195d05fbc9723f2c40958
AmulyaChaya <amulyahn2012@gmail.com>
2021-07-21 15:59:23 +0530

    Update sendoffUser.groovy

--- vars/sendoffUser.groovy
+++ vars/sendoffUser.groovy
@@ -1,6 +1,6 @@
   
 def call(String username = 'null', String designation = 'null') {
-	echo "This is shared library sendoffUser: ${username}"
+	//echo "This is shared library sendoffUser: ${username}"
 	pipeline {
 		environment {
 			USER_NAME = "$username"
